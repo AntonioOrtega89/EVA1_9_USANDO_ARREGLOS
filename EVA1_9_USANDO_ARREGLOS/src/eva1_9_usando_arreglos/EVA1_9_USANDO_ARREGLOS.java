ed3272efd15a9a10252d4062eacc2ce6eaf05472
AntonioOrtega89 <82838094+AntonioOrtega89@users.noreply.github.com>
2021-10-24 19:27:41 -0500

    Add files via upload

---
+++ EVA1_9_USANDO_ARREGLOS/src/eva1_9_usando_arreglos/EVA1_9_USANDO_ARREGLOS.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_9_usando_arreglos;
+
+/**
+ *
+ * @author JOSE ALBERTO PEREZ
+ */
+public class EVA1_9_USANDO_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here //1. UN ARREGLO DE ENTEROS
+        int[] aiDatos = new int[10];
+        //2. LLENAR VALORES
+        //Arreglo: estructura de acceso aleatorio
+        //indice
+        // Primer elemento del arreglo: posicion 0
+        // Ultimo elemento del arreglo: posicion N - 1
+        //ACCESO A LOS VALORES:
+
+        for(int i = 0; i < 10; i++){
+           aiDatos[i] = (int)(Math.random() * 100);
+        }
+        //3. IMPRIMIR LOS VALORES
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("[" + aiDatos[i] + "]");
+        }    
+    }
+    
+}
